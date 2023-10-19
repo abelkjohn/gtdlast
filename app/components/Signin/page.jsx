051628d1be5331e6728653cc7d183e83a7087dff
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-10-19 19:28:52 +0530

    new file:   app/components/Signin/page.jsx 	new file:   app/components/Signup/page.jsx 	modified:   app/globals.css 	modified:   app/page.tsx

---
+++ app/components/Signin/page.jsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+//import { auth } from '../firebase';
+import { useRouter } from 'next/navigation';
+
+
+export default function HelloWorld(){
+  const [ email, setEmail ] = React.useState()
+  const [ password, setpassword ] = React.useState()
+
+  const router = useRouter()
+  function createUser(e){
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        router.push('/')
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+
+    document.getElementById('sign-in-email').value = ""
+    document.getElementById('sign-in-password').value = ""
+  }
+
+  return (
+    <form id='sign-up-box' className='sign-up-in p-10 h-3/5 rounded-2xl flex flex-col border-grey-100 border-2 shadow-lg shadow-blue-500/40'>
+      <h1 className='text-center text-2xl font-bold mb-8'>Sign-In with Email</h1>
+      <input className='p-2 rounded-2xl bg-gray-100 mb-5' type='email' id='sign-in-email' onChange={e => setEmail(e.target.value)} placeholder='Please enter email'></input>
+      <input className='p-2 rounded-2xl bg-gray-100 mb-10 ' id='sign-in-password' onChange={e => setpassword(e.target.value)} type='password' placeholder='Please enter password'></input>
+      <Link href='/components/Signup'>
+        <p className='text-sm text-center'>Don&apos;t have an account? Create one here</p>
+      </Link>
+      <button className='p-2 bg-blue-500 rounded-2xl w-3/6 m-auto text-white shadow-lg shadow-blue-500/50' onClick={createUser}>Submit</button>
+    </form>
+  )
+}
---
+++ app/components/Signup/page.jsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+//import { UserAuth } from '../context/AuthContext'
+
+
+export default function HelloWorld(){
+  const [ email, setEmail ] = React.useState()
+  const [ password, setpassword ] = React.useState()
+  const [ error, setError ] = React.useState()
+
+  const router = useRouter()
+  //const { createUser } = UserAuth()
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    setError('')
+    try{
+      await createUser(email, password)
+    } catch {
+      setError(e.message)
+      
+    }
+    document.getElementById('temp').value = error
+    document.getElementById('sign-up-email').value = ""
+    document.getElementById('sign-up-password').value = ""
+    router.push('/signin')
+  }
+
+  return (
+    <form id='sign-up-box' className='sign-up-in p-10 rounded-2xl flex flex-col border-grey-100 border-2 shadow-lg shadow-blue-500/40'>
+      <h1 className='text-center text-2xl font-bold mb-8'>Sign-Up with Email</h1>
+      <input className='p-2 rounded-2xl bg-gray-100 mb-5' id='sign-up-email' type='email' onChange={e => setEmail(e.target.value)} placeholder='Please enter email'></input>
+      <input className='p-2 rounded-2xl bg-gray-100 mb-10 ' id='sign-up-password' onChange={e => setpassword(e.target.value)} type='password' placeholder='Please enter password'></input>
+      <Link href='/components/Signin'>
+      <p className='text-sm text-center'>Already have an account? Log in here</p>
+      <p id='temp'></p>
+      </Link>
+      <button className='p-2 bg-blue-500 rounded-2xl w-3/6 m-auto text-white shadow-lg shadow-blue-500/50' onClick={handleSubmit}>Submit</button>
+    </form>
+  )
+}
