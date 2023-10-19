423190694c3b30f067d09359e5423ceb96e23b18
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-10-19 20:07:12 +0530

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import React from 'react'
+import { useRouter } from "next/navigation"
 
 const page = () => {
+  const router = useRouter()
+
+  router.push('/components/Signin')
   return (
     <div>page</div>
   )
