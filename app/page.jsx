ee77a01d4bbf9bb30fc94a11dca572f467a028d5
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-10-20 11:14:44 +0530

    modified:   app/components/Signup/page.jsx 	new file:   app/components/context/AuthContext.js 	modified:   app/layout.tsx 	modified:   app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -9,10 +9,10 @@ import { signInWithEmailAndPassword } from 'firebase/auth/cordova';
 
 export default function HelloWorld(){
   const [ email, setEmail ] = React.useState()
-  const [ password, setpassword ] = React.useState()
+  const [ password, setPassword ] = React.useState()
 
   const router = useRouter()
-  function createUser(e){
+  function logInUser(e){
     e.preventDefault()
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -34,11 +34,11 @@ export default function HelloWorld(){
     <form id='sign-up-box' className='fixed left-2/4 top-2/4 center-align sign-up-in p-10 h-3/5 rounded-2xl flex flex-col border-grey-100 border-2 shadow-lg shadow-blue-500/40'>
       <h1 className='text-center text-2xl font-bold mb-8'>Sign-In with Email</h1>
       <input className='p-2 rounded-2xl bg-gray-100 mb-5' type='email' id='sign-in-email' onChange={e => setEmail(e.target.value)} placeholder='Please enter email'></input>
-      <input className='p-2 rounded-2xl bg-gray-100 mb-10 ' id='sign-in-password' onChange={e => setpassword(e.target.value)} type='password' placeholder='Please enter password'></input>
+      <input className='p-2 rounded-2xl bg-gray-100 mb-10 ' id='sign-in-password' onChange={e => setPassword(e.target.value)} type='password' placeholder='Please enter password'></input>
       <Link href='/components/Signup'>
         <p className='text-sm text-center'>Don&apos;t have an account? Create one here</p>
       </Link>
-      <button className='p-2 bg-blue-500 rounded-2xl w-3/6 m-auto text-white shadow-lg shadow-blue-500/50' onClick={createUser}>Submit</button>
+      <button className='p-2 bg-blue-500 rounded-2xl w-3/6 m-auto text-white shadow-lg shadow-blue-500/50' onClick={logInUser}>Submit</button>
     </form>
   )
 }
