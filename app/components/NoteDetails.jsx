a825fad6d786813b4c207a55d8130262a9c25200
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-10-28 19:47:42 +0530

    modified:   app/components/NoteDetails.jsx

--- app/components/NoteDetails.jsx
+++ app/components/NoteDetails.jsx
@@ -13,6 +13,7 @@ export default function NoteDetails({id, bucketName}){
     const [ post, setPost ] = React.useState('')
     const [ main, setMain ] = React.useState('')
     const [ bucket, setBucket ] = React.useState('')
+    const [ bucketArray, setBucketArray ] = React.useState([])
 
     const { user } = UserAuth() 
 
@@ -88,13 +89,28 @@ export default function NoteDetails({id, bucketName}){
         })
         document.getElementById("note-specific").style.display = "none"
     }
+
+    React.useEffect(function(){
+        const email = user ? user.email.replace('.', '&dot') : ''
+
+        const buckets = ref(db, `${email}/buckets`)
+        onValue(buckets, (snapshot) => {
+            const data = snapshot.val();
+            if (data === null){
+
+            } else {
+                setBucketArray(Object.keys(data))
+            }
+        })
+    }, []) 
+
     return (
         <div id='note-specific' className='shadow-cyan-500 shadow-2xl rounded-2xl hidden fixed flex-col  top-1/2 left-1/2 center-align bg-white p-0 w-80 '>
             <div className='text-m p-2 border-cyan-500 border-2 overflow-hidden rounded-xl rounded-b-none border-b-0 flex flex-col'>
                 <input className='wrap mr-5 m-1 no-focus' id='edit-post' onChange={e => setPost(e.target.value)} placeholder='Add your Title here' value={post}></input>
-                <input className='m-1 no-focus' value={bucket} onChange={e => setBucket(e.target.value)} placeholder='Please Enter the bucket to transfer' list='buckets-info'></input>
+                <input className='m-1 no-focus' onChange={e => setBucket(e.target.value)}  placeholder={bucketName ? bucketName : 'Please input bucket to transfer'} list='buckets-info'></input>
                 <datalist id='buckets-info'>
-                    <option value="* Next Actions *"/>
+                    {bucketArray.length > 0 ? bucketArray.map(i => <option key={i} value={i}/> ) : null}
                 </datalist>
                 <textarea className='m-1 h-80 no-focus mb-0' id='edit-main' onChange={e => setMain(e.target.value)} placeholder='Add your Note here' value={main}></textarea>
             </div>
