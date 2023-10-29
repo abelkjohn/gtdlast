fb7a79d5b519eebfbd021ababb982da8841c52e8
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-10-29 21:10:43 +0530

    modified:   app/components/NoteDetails.jsx 	modified:   app/globals.css

--- app/components/NoteDetails.jsx
+++ app/components/NoteDetails.jsx
@@ -108,15 +108,15 @@ export default function NoteDetails({id, bucketName}){
         <div id='note-specific' className='shadow-cyan-500 shadow-2xl rounded-2xl hidden fixed flex-col  top-1/2 left-1/2 center-align bg-white p-0 w-80 '>
             <div className='text-m p-2 border-cyan-500 border-2 overflow-hidden rounded-xl rounded-b-none border-b-0 flex flex-col'>
                 <input className='wrap mr-5 m-1 no-focus' id='edit-post' onChange={e => setPost(e.target.value)} placeholder='Add your Title here' value={post}></input>
-                <input className='m-1 no-focus' onChange={e => setBucket(e.target.value)}  placeholder={bucketName ? bucketName : 'Please input bucket to transfer'} list='buckets-info'></input>
+                <input className='m-1 no-focus' onChange={e => setBucket(e.target.value)} value={bucket ? bucket : null}  placeholder={bucketName ? bucketName : 'Please input bucket to transfer'} list='buckets-info'></input>
                 <datalist id='buckets-info'>
                     {bucketArray.length > 0 ? bucketArray.map(i => <option key={i} value={i}/> ) : null}
                 </datalist>
                 <textarea className='m-1 h-80 no-focus mb-0' id='edit-main' onChange={e => setMain(e.target.value)} placeholder='Add your Note here' value={main}></textarea>
             </div>
-            <div className='overflow-hidden flex justify-around m-0 border-r-green-600 border-l-red-600  rounded-b-xl'>
-                <button className='text-white text-lg no-focus w-full p-4 bg-red-600 ' onClick={removeNote}>Delete</button>
-                <button className="no-focus text-white text-lg bg-green-600 p-4 m-0 w-full" onClick={saveNote}>Save</button>
+            <div className='rounded-b-15px overflow-hidden flex justify-around m-0 border-r-green-600 border-l-red-600  rounded-b-xl'>
+                <button className='rounded-bl-15px text-white text-lg no-focus w-full p-4 bg-red-600 ' onClick={removeNote}>Delete</button>
+                <button className="rounded-br-15px no-focus text-white text-lg bg-green-600 p-4 m-0 w-full" onClick={saveNote}>Save</button>
             </div>
             <button onClick={closePopup} className='text-red-600 fixed right-2 top-1 text-2xl'>X</button>
         </div>
