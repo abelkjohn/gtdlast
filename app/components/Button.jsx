0c1f4138f3374942e802e63e604ba43f5396b6be
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-12-18 08:48:02 +0530

    modified:   app/components/Button.jsx

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Button(){
-    return <h1>button</h1>
+    return <h1 className="w-20 h-10 p-1 text-center fixed right-2 bottom-2 bg-white">/\</h1>
 }
